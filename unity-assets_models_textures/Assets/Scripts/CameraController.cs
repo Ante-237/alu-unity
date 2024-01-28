ce554fbd84cf285740ec17a61e3894dfb5fcba4b
Ante-237 <akumawahanye@gmail.com>
2024-01-29 01:22:39 +0200

    update to camera follow script.

--- unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -1,20 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
-    private GameObject Player;
+    [SerializeField] private GameObject Player;
+    [SerializeField] private float AngelRotation = 45f;
     
-    
-    void Update()
+    private Vector3 DistanceApart = Vector3.zero;
+
+
+    private void Awake()
     {
-        
+        DistanceApart =  transform.position - Player.transform.position;
     }
 
 
+    private void Update()
+    {
+        FollowPlayer();
+        RotateAroundPlayer();
+    }
+
+    void RotateAroundPlayer()
+    {
+        float RotationYDirection = Input.GetAxis("Mouse X");
+        transform.RotateAround(Player.transform.position, Vector3.up, RotationYDirection * RotationYDirection);
+    }
+
     void FollowPlayer()
     {
-        transform.position = Player.transform.position + transform.position;
+        transform.position = Player.transform.position + DistanceApart;
     }
 }
--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -8,8 +9,9 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody rb;
-    private float JumpForce = 500f;
-    private float MovementSpeed = 20f;
+    [SerializeField] private float JumpForce = 300f;
+    
+    [SerializeField] private float MovementSpeed = 20f;
     private Vector3 vertical;
     private Vector3 horizontal;
     private float directionX;
@@ -32,8 +34,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector3(transform.position.x, transform.position.y * JumpForce * Time.deltaTime, transform.position.z);
-            
+            rb.AddForce(Vector3.up * JumpForce);
         }
        
     }
@@ -44,8 +45,9 @@ public class PlayerController : MonoBehaviour
         directionX = Input.GetAxisRaw("Horizontal");
         directionZ = Input.GetAxisRaw("Vertical");
 
-        Vector3 movements = new Vector3(directionX , 0, directionZ ) * (Time.deltaTime * MovementSpeed);
-        transform.position += movements;
+        Vector3 movements = new Vector3(directionX, 0, directionZ);//* (Time.deltaTime * MovementSpeed);
+        //transform.position += movements;
+        rb.AddForce( movements * (Time.deltaTime * MovementSpeed));
 
     }
 
