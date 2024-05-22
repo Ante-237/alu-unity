880a108cbbc766b0b0299dc5d0069c0e65f052e1
Ante-237 <a.nwalahnjie@alustudent.com>
2024-05-22 13:15:41 +0200

    Sound check
    Hover and click sound events.

---
+++ unity-audio/Assets/Scripts/ClickSoundPlaying.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickSoundPlaying : MonoBehaviour
+{
+    public Animator ClickAnimator;
+    public Button button;
+
+
+    private void Update()
+    {
+           }
+}
